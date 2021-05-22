6b97a449160108177a563f22a82dce56cf53ea9b
Kneelawk <kneelawk@gmail.com>
2021-05-22 04:43:19 -0700

    Initial implementation and rename to media-server

---
+++ src/config.rs
@@ -0,0 +1,75 @@
+use derive_more::From;
+use regex::RegexSet;
+use serde::{Deserialize, Serialize};
+use std::{
+    fmt::{Display, Formatter},
+    fs::File,
+    io::Read,
+};
+
+const CONFIG_FILE_NAME: &str = "media-server-config.toml";
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+struct ConfigRaw {
+    general: ConfigGeneral,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+struct ConfigGeneral {
+    #[serde(rename = "base-dir")]
+    base_dir: String,
+    #[serde(rename = "exclude-patterns", default)]
+    exclude_patterns: Vec<String>,
+    #[serde(default = "default_bindings")]
+    bindings: Vec<String>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Config {
+    pub base_dir: String,
+    pub exclude_patterns: RegexSet,
+    pub bindings: Vec<String>,
+}
+
+impl Config {
+    pub fn load() -> Result<Config, ConfigLoadError> {
+        let mut cfg_file = File::open(CONFIG_FILE_NAME)?;
+        let mut cfg_string = String::new();
+        cfg_file.read_to_string(&mut cfg_string)?;
+
+        let cfg_raw: ConfigRaw = toml::from_str(&cfg_string)?;
+
+        Ok(Config {
+            base_dir: cfg_raw.general.base_dir,
+            exclude_patterns: RegexSet::new(cfg_raw.general.exclude_patterns)?,
+            bindings: cfg_raw.general.bindings,
+        })
+    }
+}
+
+#[derive(Debug, From)]
+pub enum ConfigLoadError {
+    IOError(std::io::Error),
+    DeserializeError(toml::de::Error),
+    RegexError(regex::Error),
+}
+
+impl Display for ConfigLoadError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ConfigLoadError::IOError(e) => {
+                write!(
+                    f,
+                    "Unable to open config file: '{}': {}",
+                    CONFIG_FILE_NAME, e
+                )
+            }
+            ConfigLoadError::DeserializeError(_) => write!(f, "Unable to parse config file."),
+            ConfigLoadError::RegexError(_) => write!(f, "Regex error in config file."),
+        }
+    }
+}
+
+fn default_bindings() -> Vec<String> {
+    return vec!["127.0.0.1:9090".to_string()];
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
-use actix_web::{App, HttpServer};
+mod config;
+
+use crate::config::Config;
 use actix_files::Files;
+use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(Files::new("/files", "/media/jedidiah/anime")))
-        .bind("127.0.0.1:9090")?
-        .run()
-        .await
+    dotenv::dotenv().ok();
+    env_logger::init();
+
+    let config = match Config::load() {
+        Ok(it) => it,
+        Err(err) => {
+            eprintln!("Error loading config file: {}", err);
+            return Ok(());
+        }
+    };
+
+    let base_dir = config.base_dir.clone();
+
+    let mut server = HttpServer::new(move || {
+        let base_dir = base_dir.clone();
+
+        App::new().service(Files::new("/files", base_dir).show_files_listing())
+    });
+
+    for binding in config.bindings.iter() {
+        server = server.bind(binding.clone())?;
+    }
+
+    server.run().await
 }
