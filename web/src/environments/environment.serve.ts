4fbbc7295f8e3c44e295c75e0e203254c9611339
Kneelawk <kneelawk@gmail.com>
2021-05-29 10:09:58 -0700

    Add serve configuration to angular project

---
+++ web/src/environments/environment.serve.ts
@@ -0,0 +1,10 @@
+// Debug server mode.
+// This mode is designed for rapid development. This includes using 'localhost:9090' as the api target instead of
+// wherever this application is served from.
+
+export const environment = {
+  production: false,
+  serve: true
+};
+
+import 'zone.js/dist/zone-error';
