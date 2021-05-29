07b5cd2fa509ea9d482319211712ab4aa278a37b
Kneelawk <kneelawk@gmail.com>
2021-05-29 11:21:10 -0700

    Copy html from the old anime-server

---
+++ web/src/app/menu-bar/menu-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-bar',
+  templateUrl: './menu-bar.component.html',
+  styleUrls: ['./menu-bar.component.styl']
+})
+export class MenuBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
